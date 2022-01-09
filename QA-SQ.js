1615b07e163bb551f7922c1dd0604104a9584c07
Randy Queen <96462470+dewmin123@users.noreply.github.com>
2022-01-09 10:37:41 +0530

    Create QA-SQ.js

---
+++ QA-SQ.js
@@ -0,0 +1,5 @@
+module.exports = {
+    RANDY QUEEN__OWNER: '94757405652', // Black Amda - Queen Amdi Owner
+    RANDY QUEEN__TEAM: '94763031737,94759600549,94726612855,94775812662,94762888500', // Team - RANDY Queen  TEAM
+    SEW_QUEEN_OWNER: '94785435462' // Ravindu Manoj - Sew Queen Owner --> For The Friendship 
+};
